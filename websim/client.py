a7d170d5405263b36314513fe8eafcd910627b5a
pmankevich <pmankevich@yandex-team.ru>
2018-08-19 01:33:48 +0300

    refactoring

--- websim/client.py
+++ websim/client.py
@@ -1,56 +1,44 @@
-
 # coding: utf-8
 
-# In[1]:
-
-import time
 import datetime
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import NoSuchElementException
-
-import numpy as np
 import pandas as pd
+import time
+import os
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from constants import alpha_stats
 
 
-# In[2]:
+class WebSim(object):
 
-alpha_stats = ['alpha', 'year', 'long_count', 'short_count', 'pnl', 'sharpe', 'fitness', 
-               'returns', 'draw_down', 'turn_over', 'margin']
-
-
-# In[15]:
-
-class WebSim():
-    
     def __init__(self, implicitly_wait=60):
-        self.driver = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
+        self.driver = webdriver.Firefox()
         self.driver.set_window_size(1366, 768)
         self.driver.implicitly_wait(implicitly_wait)
         self.date = datetime.datetime.now().__str__().split()[0]
+        self._login = os.environ['WEBSIM_LOGIN']
+        self._password = os.environ['WEBSIM_PASSWORD']
 
-    def __del__(self):
-        self.driver.quit()
-        
     def login(self, relog=False):
         if relog:
             self.driver.get('https://websim.worldquantchallenge.com/logout')
-        
+
         self.driver.get('https://websim.worldquantchallenge.com/en/cms/wqc/websim/')
-        log_pass= self.driver.find_elements_by_class_name('form-control')
-        log_pass[0].clear(), log_pass[0].send_keys('mankevichpavel@gmail.com')
-        log_pass[1].clear(), log_pass[1].send_keys('googlehf,jnf')
+        log_pass = self.driver.find_elements_by_class_name('form-control')
+        log_pass[0].clear(), log_pass[0].send_keys(self._login)
+        log_pass[1].clear(), log_pass[1].send_keys(self._password)
         log_pass[1].send_keys(Keys.RETURN)
-        
+
         self.login_time = time.time()
         time.sleep(10)
-        #find dashboard element
-        
+
     def stats(self, i, alpha):
         table = self.driver.find_elements_by_class_name('standard-row')
         for row_id, row in enumerate(table):
@@ -66,20 +54,18 @@ class WebSim():
             self.res_df.draw_down.iloc[i * 7 + row_id] = data[9].text
             self.res_df.turn_over.iloc[i * 7 + row_id] = data[10].text
             self.res_df.margin.iloc[i * 7 + row_id] = data[11].text
-           
-        
+
     def simulate(self, alphas_df, res_df=None, i_start=None):
         if res_df is None:
             res_df = pd.DataFrame(index=range(alphas_df.shape[0] * 7), columns=alpha_stats)
-        
+
         if i_start is None:
             i_start = res_df.dropna(how='all').shape[0] / 7
-            #i_start = (res_df.dropna(how='all').index[-1] + 1) / 7
-            
+
         self.res_df = res_df
         self.alphas_df = alphas_df
-        
-        while(1):
+
+        while True:
             try:
                 for i in range(alphas_df.shape[0])[i_start:]:
                     alpha = alphas_df.iloc[i][0]
@@ -93,12 +79,10 @@ class WebSim():
 
                     self.driver.find_elements_by_class_name('col-xs-4')[2].click()
                     self.driver.find_element_by_id('test-statsBtn').click()
-                    
+
                     self.stats(i, alpha)
                     if i % 30 == 0:
-                        #save_df.iloc[save_start * 7:(i + 1) * 7] = res_df.iloc[save_start * 7:(i + 1) * 7]
                         res_df.to_csv(self.date + '_simulate.csv', index=False)
-                        save_start = i + 1
 
                     if int(time.time()) - self.login_time > 10800:
                         self.login(relog=True)
@@ -109,39 +93,30 @@ class WebSim():
                     i_start = i + 1
                 else:
                     i_start = i
-            
+
             if i == alphas_df.shape[0] - 1:
                 res_df.to_csv(self.date + '_simulate.csv', index=False)
                 break
-               
-            
+
     def error(self, error, i):
         if 'CodeMirror-line' in error.msg:
             self.driver.get('https://websim.worldquantchallenge.com/simulate')
             try:
                 element_present = EC.presence_of_element_located((By.CLASS_NAME, 'CodeMirror-line'))
                 WebDriverWait(self.driver, 120).until(element_present)
-                #WebDriverWait(self.driver, 120).until(element_present).click()
+                # WebDriverWait(self.driver, 120).until(element_present).click()
                 return True
-                
+
             except TimeoutException:
                 self.login(relog=True)
                 return True
-    
+
         if 'test-statsBtn' in error.msg:
             try:
                 element_present = EC.presence_of_element_located((By.ID, 'test-statsBtn'))
                 WebDriverWait(self.driver, 180).until(element_present)
-                #WebDriverWait(self.driver, 180).until(element_present).click()
                 return True
-                
+
             except TimeoutException:
                 self.login(relog=True)
                 return False
-                
-
-
-# In[ ]:
-
-
-
---
+++ websim/constants.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+
+alpha_stats = ['alpha', 'year', 'long_count', 'short_count', 'pnl', 'sharpe', 'fitness',
+               'returns', 'draw_down', 'turn_over', 'margin']
